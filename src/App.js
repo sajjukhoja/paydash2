6910ff3babbe6e12d9cc40336874c1c03015d5c9
sajjukhoja <sajjadalikhoja@gmail.com>
2019-07-03 00:11:58 -0400

    first big commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './Components/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import NavBar from './Small.Components/Drawer.js'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+}));
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar / >
+    </div>
+  );
+}
